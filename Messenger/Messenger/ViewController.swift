e2f4f4746c9a26cad77f7a23651ad01350db1f1c
Vũ Tựu <vu.van.tuu@rue.co.jp>
2020-08-22 14:50:10 +0700

    setup

---
+++ Messenger/Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Vũ Tựu on 8/22/20.
+//  Copyright © 2020 Vũ Tựu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
